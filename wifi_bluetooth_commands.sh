aa0f400da39e79b74a0cccee2510f56c9be6e994
AmmarHUSSEIN99 <hussein.ammar898@gmail.com>
2025-07-28 03:31:35 +0200

    Add all Linux commands used in Lab

---
+++ wifi_bluetooth_commands.sh
@@ -0,0 +1,24 @@
+# Network interface info
+ifconfig
+iwconfig
+ip a
+iw dev
+
+# Wi-Fi scanning
+iwlist wlan0 scan
+iw wlan0 scan
+iw wlan0 link
+
+# Routing and network analysis
+ip route show
+traceroute 8.8.8.8
+
+# Bluetooth Classic
+hciconfig
+hcitool dev
+hcitool scan
+
+# Bluetooth Low Energy
+btmgmt le on
+btmgmt find
+hcitool lescan
